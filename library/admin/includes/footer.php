bde5e4492c52672e2f1473f86c85590de2e75ad5
Mohdanasshariq <mohdanasshariq@gmail.com>
2023-05-24 14:41:20 +0530

    init

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Seminar Library Website |<a href="https://amu.ac.in/" target="_blank"  > Designed by : Mohd Anas Shariq</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
